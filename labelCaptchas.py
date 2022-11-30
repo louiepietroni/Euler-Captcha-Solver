69d90440d057cc16057944ac1a71f1069be56209
louiepietroni <lp599@cam.ac.uk>
2022-11-30 20:51:08 +0000

    Refined method of labelling captchas with better UI and not bothering downloading large numbers of files and saving, just pulling them as needed

--- labelCaptchas.py
+++ labelCaptchas.py
@@ -1,9 +1,5 @@
 import captchautils
-import cv2
 import numpy as np
-
-file_path = 'Captchas/'
-start_index = 146
 
 
 def save_digits_to_file(images, values):
@@ -36,35 +32,12 @@ def get_digits_in_file():
         print('Training digits:', len(lines))
 
 
-# index = start_index
-# while True:
-#     # Load the next captcha
-#     file_name = 'captcha%d.png' % index
-#     captcha = cv2.imread(file_path + file_name)
-#     if captcha is None:
-#         print("Failed to load image")
-#         exit(-1)
-#
-#     digits = captchautils.get_digits_from_captcha(captcha)
-#     prepped_digits = [captchautils.scale_and_grey_digit(digit) for digit in digits]
-#
-#     cv2.imshow('Captcha', captchautils.get_scaled_image(captcha, 2))
-#     cv2.waitKey(1)
-#     answer = input("Value:")
-#     if len(answer) != 5:
-#         print('Next index to do:', index)
-#         break
-#     save_digits_to_file(prepped_digits, answer)
-#     index += 1
-
 while True:
     captcha = captchautils.get_new_captcha()
 
     digits = captchautils.get_digits_from_captcha(captcha, show=True)
     prepped_digits = [captchautils.scale_and_grey_digit(digit) for digit in digits]
 
-    # cv2.imshow('Captcha', captchautils.get_scaled_image(captcha, 2))
-    # cv2.waitKey(1)
     answer = input("Enter Captcha value:")
     if answer == 'skip':
         print('Skipping this captcha')
@@ -80,6 +53,3 @@ while True:
         save_digits_to_file(prepped_digits, answer)
     else:
         break
-
-
-
