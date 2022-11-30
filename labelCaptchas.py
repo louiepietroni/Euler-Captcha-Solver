5fd6fa07c6d254af55b273ed3d571c6462d6b368
louiepietroni <lp599@cam.ac.uk>
2022-11-30 20:56:27 +0000

    Added comments for the label captcha functions

--- labelCaptchas.py
+++ labelCaptchas.py
@@ -3,6 +3,12 @@ import numpy as np
 
 
 def save_digits_to_file(images, values):
+    """
+    Takes in 5 digit images and their corresponding values and appends them to the training file
+    :param images: List of 5 numpy arrays, each representing a digit
+    :param values: 5 character string labelling the digits
+    :return: None
+    """
     with open('training.txt', 'a') as labelled_digit_file:
         for i in range(5):
             digit_image = np.array(images[i].flatten())
@@ -14,6 +20,10 @@ def save_digits_to_file(images, values):
 
 
 def remove_digits_from_file():
+    """
+    Removes the 5 last lines from the training file (representing the most recent captcha)
+    :return: None
+    """
     with open('training.txt', 'r+') as file:
         lines = file.readlines()
         # move file pointer to the beginning of a file
@@ -21,23 +31,27 @@ def remove_digits_from_file():
         # truncate the file
         file.truncate()
 
-        # start writing lines except the first line
-        # lines[1:] from line 2 to last line
         file.writelines(lines[:-5])
 
 
 def get_digits_in_file():
+    """
+    Print the number of training digits we have recorded so far
+    :return: None
+    """
     with open('training.txt', 'r') as file:
         lines = file.readlines()
         print('Training digits:', len(lines))
 
 
 while True:
+    # Get a new captcha
     captcha = captchautils.get_new_captcha()
-
+    # Split it into its digits (which shows it also) and get the prepped digits too
     digits = captchautils.get_digits_from_captcha(captcha, show=True)
     prepped_digits = [captchautils.scale_and_grey_digit(digit) for digit in digits]
 
+    # Get the user to label the captcha, then take the corresponding step
     answer = input("Enter Captcha value:")
     if answer == 'skip':
         print('Skipping this captcha')
